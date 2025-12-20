abb143e7899f8a78aededb5e79a4dbdac01eed3f
Dheeraj Goswami <dheerajgoswami2004@gmail.com>
2025-12-20 21:51:12 +0530

    Changed anchor to button

--- frontend/src/landing/common/Navbar.jsx
+++ frontend/src/landing/common/Navbar.jsx
@@ -36,13 +36,12 @@ function Navbar() {
                         <li className="cursor-pointer hover:text-blue-600 transition-colors duration-200">
                             {" "}
                             {user ? (
-                                <a
-                                    href={DASHBOARD_URL}
+                                <button
                                     onClick={handleDashboardRedirect}
                                     className="hover:text-red-500 font-medium"
                                 >
                                     Dashboard
-                                </a>
+                                </button>
                             ) : (
                                 <Link to="/signup">Signup</Link>
                             )}
